e32f7712dd141a147b333de60c65c8e60867c231
Ivan Krivonos <devbackend@yandex.ru>
2021-10-19 07:54:45 +0300

    Implement simple map-storage

---
+++ internal/storage/storage.go
@@ -0,0 +1,53 @@
+package storage
+
+import (
+	"sync"
+)
+
+type Storage struct {
+	mx   sync.RWMutex
+	keys map[string]interface{}
+}
+
+func NewStorage() *Storage {
+	return &Storage{
+		mx:   sync.RWMutex{},
+		keys: map[string]interface{}{},
+	}
+}
+
+func (s *Storage) Get(key string) (interface{}, bool) {
+	s.mx.RLock()
+	val, ok := s.keys[key]
+	s.mx.RUnlock()
+
+	return val, ok
+}
+
+func (s *Storage) Set(key string, value interface{}) error {
+	s.mx.Lock()
+	s.keys[key] = value
+	s.mx.Unlock()
+
+	return nil
+}
+
+func (s *Storage) Delete(key string) error {
+	s.mx.Lock()
+	delete(s.keys, key)
+	s.mx.Unlock()
+
+	return nil
+}
+
+func (s *Storage) Keys() []string {
+	var i int
+
+	res := make([]string, len(s.keys))
+	for k := range s.keys {
+		res[i] = k
+		i++
+	}
+
+	return res
+}
---
+++ internal/storage/storage_test.go
@@ -0,0 +1,105 @@
+package storage_test
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/devbackend/crutch-db/internal/storage"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStorage_GetFromEmpty(t *testing.T) {
+	st := storage.NewStorage()
+
+	_, ok := st.Get("any")
+
+	assert.Equal(t, false, ok)
+}
+
+func TestStorage_ReadSetValue(t *testing.T) {
+	st := storage.NewStorage()
+
+	err := st.Set("test", 123)
+
+	assert.Equal(t, nil, err)
+
+	val, ok := st.Get("test")
+
+	assert.Equal(t, 123, val)
+	assert.Equal(t, true, ok)
+}
+
+func TestStorage_DeleteValue(t *testing.T) {
+	st := storage.NewStorage()
+
+	_ = st.Set("test", 123)
+	err := st.Delete("test")
+
+	assert.Equal(t, nil, err)
+
+	val, ok := st.Get("test")
+
+	assert.Equal(t, nil, val)
+	assert.Equal(t, false, ok)
+}
+
+func TestStorage_KeysOnEmpty(t *testing.T) {
+	st := storage.NewStorage()
+
+	assert.Equal(t, []string{}, st.Keys())
+}
+
+func TestStorage_Keys(t *testing.T) {
+	st := storage.NewStorage()
+
+	_ = st.Set("test1", 123)
+	_ = st.Set("test2", 456)
+	_ = st.Set("test3", 789)
+
+	actual := st.Keys()
+
+	sort.Strings(actual)
+
+	assert.Equal(t, []string{"test1", "test2", "test3"}, actual)
+}
+
+func BenchmarkStorageSet_RewriteKey(b *testing.B) {
+	benchmarks := []struct {
+		name  string
+		value interface{}
+	}{
+		{
+			name:  "small string",
+			value: string(make([]byte, 64)),
+		},
+		{
+			name:  "big string",
+			value: string(make([]byte, 1024*1024)),
+		},
+		{
+			name:  "small int slice",
+			value: make([]int, 64),
+		},
+		{
+			name:  "big int slice",
+			value: make([]int, 1024*1024),
+		},
+		{
+			name:  "small string slice",
+			value: make([]string, 64),
+		},
+		{
+			name:  "big string slice",
+			value: make([]string, 1024*1024),
+		},
+	}
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			st := storage.NewStorage()
+
+			for i := 0; i < b.N; i++ {
+				_ = st.Set("test-key", bm.value)
+			}
+		})
+	}
+}
